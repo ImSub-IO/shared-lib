68e7e0f095f8e450dc6981a90f8adcf16b56c73c
Marco <cavallo.marco0@gmail.com>
2022-02-17 14:35:42 +0100

    Added new error code

--- C#/ImSubShared/ImSubShared.APIErrorResponses/SubCode.cs
+++ C#/ImSubShared/ImSubShared.APIErrorResponses/SubCode.cs
@@ -12,10 +12,13 @@
         TELEGRAM_INVALID_DATA = 100,
         TELEGRAM_ID_NOT_FOUND = 101,
 
-        // Authentication error codes (from 200 to 300)
+        // Authentication error codes (from 200 to 299)
         REFRESH_TOKEN_REQUIRED = 200,
         INVALID_REFRESH_TOKEN = 201,
-        MAIL_ALREADY_PRESENT = 202
+        MAIL_ALREADY_PRESENT = 202,
+
+        // User codes (from 300 to 399)
+        USER_ALREADY_REGISTERED = 300
         
     }
 }
