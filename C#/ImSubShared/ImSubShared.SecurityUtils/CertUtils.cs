62f3c73f6481424c377b715518941a5a036c76e8
Marco <cavallo.marco0@gmail.com>
2022-03-28 17:46:01 +0200

    Fixed bugs

--- C#/ImSubShared/ImSubShared.SecurityUtils/CertUtils.cs
+++ C#/ImSubShared/ImSubShared.SecurityUtils/CertUtils.cs
@@ -12,59 +12,67 @@ namespace ImSubShared.SecurityUtils
     public static class CertUtils
     {
         /// <summary>
-        /// Common client certificate autjentication middleware
+        /// Action for handling additional client cert validation actions
         /// </summary>
-        /// <param name="options"></param>
+        /// <param name="context"></param>
         /// <param name="validClientCertThumbprint"></param>
         /// <param name="validIntermediateThumbprint"></param>
         /// <param name="validCAThumbprint"></param>
         /// <param name="loggerFunction">Pass your logger function (using an <see cref="Action"/> makes the method reusable with many different loggers)</param>
-        public static void CertClientAuth(
-            CertificateAuthenticationOptions options,
+        public static Task OnClientCertValidated(
+            CertificateValidatedContext context,
             string validClientCertThumbprint,
             string validIntermediateThumbprint,
             string validCAThumbprint,
             Action<string, string> loggerFunction)
         {
+            var cert = context.ClientCertificate;
+            if (cert == null)
+            {
+                loggerFunction("Certificate not present", null);
+                context.Fail("Invalid certificate");
+                return Task.CompletedTask;
+            }
+            var x509Chain = new X509Chain();
+            x509Chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+            x509Chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+
+            if (!x509Chain.Build(cert) ||
+                x509Chain.ChainElements[0].Certificate.Thumbprint != validClientCertThumbprint ||
+                x509Chain.ChainElements[1].Certificate.Thumbprint != validIntermediateThumbprint ||
+                x509Chain.ChainElements[2].Certificate.Thumbprint != validCAThumbprint)
+            {
+                loggerFunction("Invalid certificate chain", string.Join("\n", x509Chain.ChainStatus));
+                context.Fail("Invalid certificate");
+                return Task.CompletedTask;
+            }
+
+            context.Success();
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Action for handling error in client cert validation
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="loggerFunction">Pass your logger function (using an <see cref="Action"/> makes the method reusable with many different loggers)</param>
+        /// <returns></returns>
+        public static Task OnClientCertError(CertificateAuthenticationFailedContext context, Action<string, string> loggerFunction)
+        {
+            loggerFunction(context.Exception.Message, context.Exception.ToString());
+            context.Fail("Invalid certificate");
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Setup basic options
+        /// </summary>
+        /// <param name="options"></param>
+        public static void SetupCertValidationOptions(CertificateAuthenticationOptions options)
+        {
             options.AllowedCertificateTypes = CertificateTypes.Chained;
             options.RevocationMode = X509RevocationMode.NoCheck;
             options.ValidateValidityPeriod = true;
-            options.Events = new CertificateAuthenticationEvents
-            {
-
-                OnAuthenticationFailed = context =>
-                {
-                    loggerFunction(context.Exception.Message, context.Exception.ToString());
-                    context.Fail("Invalid certificate");
-                    return Task.CompletedTask;
-                },
-                OnCertificateValidated = context =>
-                {
-                    var cert = context.ClientCertificate;
-                    if (cert == null)
-                    {
-                        loggerFunction("Certificate not present", null);
-                        context.Fail("Invalid certificate");
-                        return Task.CompletedTask;
-                    }
-                    var x509Chain = new X509Chain();
-                    x509Chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
-                    x509Chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
-
-                    if (!x509Chain.Build(cert) ||
-                        x509Chain.ChainElements[0].Certificate.Thumbprint != validClientCertThumbprint ||
-                        x509Chain.ChainElements[1].Certificate.Thumbprint != validIntermediateThumbprint ||
-                        x509Chain.ChainElements[2].Certificate.Thumbprint != validCAThumbprint)
-                    {
-                        loggerFunction("Invalid certificate chain", string.Join("\n", x509Chain.ChainStatus));
-                        context.Fail("Invalid certificate");
-                        return Task.CompletedTask;
-                    }
-
-                    context.Success();
-                    return Task.CompletedTask;
-                }
-            };
         }
     }
 }
